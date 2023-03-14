c24b82c64e17d4f7a96686af3410620b87a06e4d
albayrakibrahim3457 <77547591+albayrakibrahim3457@users.noreply.github.com>
2023-03-14 17:59:02 +0300

    generic 1

---
+++ Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("")
+        }
+    }
+    class MyList<T>  //Generic class
+    {
+        public void Add(T item)
+        {
+
+        }
+    }
+}
